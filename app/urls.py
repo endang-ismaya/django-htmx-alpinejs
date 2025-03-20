cb073555f859023ac76da971505f35f0ae33403a
endang-ismaya <endang.ismaya@gmail.com>
2025-03-20 13:22:28 +0700

    add CreateView

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+
 from app import views
 
 urlpatterns = [
-    path("", views.home, name="app__home"),
+    path("", views.home, name="app.home"),
+    path("articles/create/", views.create_article, name="app.create_article"),
+    path(
+        "articles/createview/",
+        views.ArticleCreateView.as_view(),
+        name="app.create_article_view",
+    ),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
+from app.forms import CreateArticleForm
 from app.models import Article
 
 
@@ -9,3 +12,33 @@ def home(request):
         "articles": articles,
     }
     return render(request, "app/home.html", context)
+
+
+class ArticleCreateView(CreateView):
+    template_name = "app/article_create.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "twitter_post"]
+    success_url = reverse_lazy("app.home")
+
+
+def create_article(request):
+    if request.method == "POST":
+        # data is submitted
+        form = CreateArticleForm(request.POST)
+        if form.is_valid():
+            fcd = form.cleaned_data()
+            new_article = Article(
+                title=fcd["title"],
+                status=fcd["status"],
+                content=fcd["content"],
+                word_count=fcd["word_count"],
+                twitter_post=fcd["twitter_post"],
+            )
+            new_article.save()
+
+            return redirect("app.home")
+
+    else:
+        form = CreateArticleForm()
+
+    return render(request, "app/article_create.html", {"form": form})
