6d8cd7f85143e6be73298bd2f82a7995026e0415
endang-ismaya <endang.ismaya@gmail.com>
2025-03-20 14:44:46 +0700

    add update, delete, detail view

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,26 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path("", views.home, name="app.home"),
-    path("articles/create/", views.create_article, name="app.create_article"),
+    path("", views.ArticleListView.as_view(), name="app.home"),
+    # path("articles/create/", views.create_article, name="app.create_article"),
     path(
-        "articles/createview/",
+        "articles/create/",
         views.ArticleCreateView.as_view(),
-        name="app.create_article_view",
+        name="app.create_article",
+    ),
+    path(
+        "articles/<int:pk>/update/",
+        views.ArticleUpdateView.as_view(),
+        name="app.update_article",
+    ),
+    path(
+        "articles/<int:pk>/delete/",
+        views.ArticleDeleteView.as_view(),
+        name="app.delete_article",
+    ),
+    path(
+        "articles/<int:pk>/detail/",
+        views.ArticleDetailView.as_view(),
+        name="app.detail_article",
     ),
 ]
--- app/views.py
+++ app/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-from app.forms import CreateArticleForm
 from app.models import Article
 
 
-def home(request):
-    articles = Article.objects.all()
-    context = {
-        "articles": articles,
-    }
-    return render(request, "app/home.html", context)
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
 
 
 class ArticleCreateView(CreateView):
@@ -21,24 +23,46 @@ class ArticleCreateView(CreateView):
     success_url = reverse_lazy("app.home")
 
 
-def create_article(request):
-    if request.method == "POST":
-        # data is submitted
-        form = CreateArticleForm(request.POST)
-        if form.is_valid():
-            fcd = form.cleaned_data()
-            new_article = Article(
-                title=fcd["title"],
-                status=fcd["status"],
-                content=fcd["content"],
-                word_count=fcd["word_count"],
-                twitter_post=fcd["twitter_post"],
-            )
-            new_article.save()
+class ArticleUpdateView(UpdateView):
+    template_name = "app/article_update.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "twitter_post"]
+    success_url = reverse_lazy("app.home")
+    context_object_name = "article"
 
-            return redirect("app.home")
 
-    else:
-        form = CreateArticleForm()
+class ArticleDeleteView(DeleteView):
+    template_name = "app/article_delete.html"
+    model = Article
+    success_url = reverse_lazy("app.home")
+    context_object_name = "article"
 
-    return render(request, "app/article_create.html", {"form": form})
+
+class ArticleDetailView(DetailView):
+    template_name = "app/article_detail.html"
+    model = Article
+    context_object_name = "article"
+    success_url = reverse_lazy("app.home")
+
+
+# def create_article(request):
+#     if request.method == "POST":
+#         # data is submitted
+#         form = CreateArticleForm(request.POST)
+#         if form.is_valid():
+#             fcd = form.cleaned_data()
+#             new_article = Article(
+#                 title=fcd["title"],
+#                 status=fcd["status"],
+#                 content=fcd["content"],
+#                 word_count=fcd["word_count"],
+#                 twitter_post=fcd["twitter_post"],
+#             )
+#             new_article.save()
+
+#             return redirect("app.home")
+
+#     else:
+#         form = CreateArticleForm()
+
+#     return render(request, "app/article_create.html", {"form": form})
