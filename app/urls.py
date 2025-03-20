999ce9fb5a375321457cd2a779178a8a58db746e
endang-ismaya <endang.ismaya@gmail.com>
2025-03-20 16:01:36 +0700

    add base template

--- app/urls.py
+++ app/urls.py
@@ -6,22 +6,22 @@ urlpatterns = [
     path("", views.ArticleListView.as_view(), name="app.home"),
     # path("articles/create/", views.create_article, name="app.create_article"),
     path(
-        "articles/create/",
+        "create/",
         views.ArticleCreateView.as_view(),
         name="app.create_article",
     ),
     path(
-        "articles/<int:pk>/update/",
+        "<int:pk>/update/",
         views.ArticleUpdateView.as_view(),
         name="app.update_article",
     ),
     path(
-        "articles/<int:pk>/delete/",
+        "<int:pk>/delete/",
         views.ArticleDeleteView.as_view(),
         name="app.delete_article",
     ),
     path(
-        "articles/<int:pk>/detail/",
+        "<int:pk>/detail/",
         views.ArticleDetailView.as_view(),
         name="app.detail_article",
     ),
