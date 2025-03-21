55dfde2c01ac4489a800838b0e07495fc99b9b94
endang-ismaya <endang.ismaya@gmail.com>
2025-03-21 14:45:02 +0700

    add creator fullname on ArticleAdmin

--- app/admin.py
+++ app/admin.py
@@ -11,13 +11,20 @@ class ArticleAdmin(admin.ModelAdmin):
         "status",
         "created_at",
         "updated_at",
-        "creator__first_name",
+        "creator_fullname",
     ]
     list_filter = ("status",)
     search_fields = ("title", "content")
     date_hierarchy = "created_at"
     ordering = ("-created_at",)
     readonly_fields = ("word_count", "created_at", "updated_at")
+
+    @admin.display(description="creator")
+    def creator_fullname(self, obj: Article):
+        if obj.creator:
+            creator: UserProfile = obj.creator
+            return f"{creator.first_name} {creator.last_name}"
+        return "No Creator"
 
 
 @admin.register(UserProfile)
