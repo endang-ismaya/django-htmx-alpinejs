05714933399ca1f7b3a4fb8610501eb6bb403c1e
endang-ismaya <endang.ismaya@gmail.com>
2025-04-08 21:14:56 +0700

    update root urls

--- src/urls.py
+++ src/urls.py
@@ -1,12 +1,14 @@
+from allauth.account.views import SignupView
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="app.home")),
+    path("", SignupView.as_view(), name="account_signup"),
     path("admin/", admin.site.urls),
     path("articles/", include("app.urls")),
     # path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup/", RedirectView.as_view(url="/", permanent=True)),
     path("accounts/", include("allauth.urls")),
     # development
     path("__reload__/", include("django_browser_reload.urls")),
